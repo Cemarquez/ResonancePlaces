1e55276084a96ced4e62b8d969d70b6333230977
Brian Giraldo <briand.giraldop@uqvirtual.edu.co>
2021-04-18 19:23:15 -0500

    repos, test y sql files correspondientes añadidos

--- persistencia/src/test/java/co/edu/uniquindio/resonance/test/CategoriaTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/resonance/test/CategoriaTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
@@ -46,6 +47,14 @@ public class CategoriaTest {
 
     @Test
     public void listarCategoriaTest(){
+        List<Categoria> lista = categoriaRepo.findAll();
+
+        System.out.println(lista);
+    }
+
+    @Test
+    @Sql({"classpath:categorias.sql"})
+    public void listarCategoriaTestSQL(){
         List<Categoria> lista = categoriaRepo.findAll();
 
         System.out.println(lista);
--- persistencia/src/test/java/co/edu/uniquindio/resonance/test/TelefonoTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/resonance/test/TelefonoTest.java
@@ -1,12 +1,15 @@
 package co.edu.uniquindio.resonance.test;
 
+import co.edu.uniquindio.resonance.entidades.Lugar;
 import co.edu.uniquindio.resonance.entidades.Telefono;
+import co.edu.uniquindio.resonance.repositorios.LugarRepo;
 import co.edu.uniquindio.resonance.repositorios.TelefonoRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
@@ -16,13 +19,25 @@ public class TelefonoTest {
 
     @Autowired
     public TelefonoRepo telefonoRepo;
+    @Autowired
+    private LugarRepo lugarRepo;
 
 
     @Test
     public void registrarTelefonoTest(){
+        Lugar lugar = new Lugar();
+        lugar.setCodigo(1);
+        lugar.setDescripcion("Mejor lugar de comidas rapidas");
+        lugar.setNombre("Chorimburger");
+        lugar.setEstado(false);
+
+        Lugar lugarGuardado = lugarRepo.save(lugar);
+        Assertions.assertNotNull(lugarGuardado);
+
         Telefono tele = new Telefono();
         tele.setCodigo(1);
         tele.setNumero("3106947999");
+        tele.setLugar(lugarGuardado);
 
         Telefono guardado = telefonoRepo.save(tele);
         Assertions.assertNotNull(guardado);
@@ -30,9 +45,19 @@ public class TelefonoTest {
 
     @Test
     public void eliminarTelefonoTest(){
+        Lugar lugar = new Lugar();
+        lugar.setCodigo(1);
+        lugar.setDescripcion("Mejor lugar de comidas rapidas");
+        lugar.setNombre("Chorimburger");
+        lugar.setEstado(false);
+
+        Lugar lugarGuardado = lugarRepo.save(lugar);
+        Assertions.assertNotNull(lugarGuardado);
+
         Telefono tele = new Telefono();
         tele.setCodigo(1);
         tele.setNumero("3106947999");
+        tele.setLugar(lugarGuardado);
 
         Telefono guardado = telefonoRepo.save(tele);
 
@@ -49,12 +74,30 @@ public class TelefonoTest {
         System.out.println(lista);
     }
 
+    @Test
+    @Sql({"classpath:categorias.sql", "classpath:usuarios.sql", "classpath:ubicaciones.sql", "classpath:lugares.sql", "classpath:telefonos.sql"})
+    public void listarTelefonoSQL(){
+        List<Telefono> lista = telefonoRepo.findAll();
+
+        System.out.println(lista);
+    }
+
 
     @Test
     public void actualizarTelefonoTest(){
+        Lugar lugar = new Lugar();
+        lugar.setCodigo(1);
+        lugar.setDescripcion("Mejor lugar de comidas rapidas");
+        lugar.setNombre("Chorimburger");
+        lugar.setEstado(false);
+
+        Lugar lugarGuardado = lugarRepo.save(lugar);
+        Assertions.assertNotNull(lugarGuardado);
+
         Telefono tele = new Telefono();
         tele.setCodigo(1);
         tele.setNumero("3106947999");
+        tele.setLugar(lugarGuardado);
 
         Telefono guardado = telefonoRepo.save(tele);
 
